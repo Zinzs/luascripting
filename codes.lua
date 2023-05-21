4cc4001abe191a7cfbd146e3b7adf5aafa2f3602
Zinzs <122226632+Zinzs@users.noreply.github.com>
2023-05-21 11:13:16 +0100

    Create codes.lua

---
+++ codes.lua
@@ -0,0 +1,11 @@
+game.StarterGui:SetCore("SendNotification", {
+        Title = "Zinc Hub ",
+        Text = 'Redeeming TRADING'
+    })
+wait(0.4)
+local args = {
+[1] = "EnterPromoCode",
+[2] = "TRADING"
+}
+game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
+end
