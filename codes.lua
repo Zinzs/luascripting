46298c58b9be74543c940632f7307386515019e2
Zinzs <122226632+Zinzs@users.noreply.github.com>
2023-06-03 13:53:40 +0100

    New code

--- codes.lua
+++ codes.lua
@@ -1,11 +1,11 @@
 game.StarterGui:SetCore("SendNotification", {
         Title = "Zinc Hub ",
-        Text = 'Redeeming TRADING'
+        Text = 'Redeeming dahoodtrades'
     })
 wait(0.4)
 local args = {
 [1] = "EnterPromoCode",
-[2] = "TRADING"
+[2] = "dahoodtrades"
 }
 game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
 
