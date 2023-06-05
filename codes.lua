e6075b08ef65a689285e2d4cc5b7e87378af5895
Zinzs <122226632+Zinzs@users.noreply.github.com>
2023-06-05 18:19:10 +0100

    new code

--- codes.lua
+++ codes.lua
@@ -19,4 +19,15 @@ local args = {
 [2] = "SUMMER2023!"
 }
 game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+        Title = "Zinc Hub ",
+        Text = 'Redeeming TRADEME!'
+    })
+wait(0.4)
+local args = {
+[1] = "EnterPromoCode",
+[2] = "TRADEME!"
+}
+game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
 
