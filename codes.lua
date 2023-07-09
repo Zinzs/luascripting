2e129235c3cc51e778187ab53b5171c7f56fb35c
Zinzs <122226632+Zinzs@users.noreply.github.com>
2023-07-09 08:14:52 +0000

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,21 +1,10 @@
 game.StarterGui:SetCore("SendNotification", {
         Title = "Zinc Hub",
-        Text = 'Redeeming FIREWORKS'
+        Text = 'Redeeming JULY2023'
     })
 wait(0.4)
 local args = {
 [1] = "EnterPromoCode",
-[2] = "FIREWORKS"
-}
-game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
-wait(1)
-game.StarterGui:SetCore("SendNotification", {
-        Title = "Zinc Hub",
-        Text = 'Redeeming 2023JULY4'
-    })
-wait(0.4)
-local args = {
-[1] = "EnterPromoCode",
-[2] = "2023JULY4"
+[2] = "JULY2023"
 }
 game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
