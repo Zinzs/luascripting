420eaaa940c9c40104eb41de08afbe3c9354a593
Zinzs <122226632+Zinzs@users.noreply.github.com>
2024-08-01 12:01:17 +0100

    updated (fr?)

--- codes.lua
+++ codes.lua
@@ -1,23 +1,29 @@
 local codes = {
-    "DRUMSHOTGUN";
     "TRADEME!";
-    "HALLOWEEN2023"; -- how does this still work 😭
     "DAUP";
-    "MELONBEAR";
     "pumpkins2023";
     "RUBY";
+    "BLAZE";
+    "PENGUIN";
+    "METAL";
+    "RACE";
 }
 
 for i, v in codes do
-wait(1)
+    wait(1)
     game.StarterGui:SetCore("SendNotification", {
-    Title = "Zinc Hub | Redeem Codes",
-    Text = 'Redeeming '.. tostring(v)
-})
-    local args = {
-        [1] = "EnterPromoCode",
-        [2] = tostring(v)
-        }
-        print(tostring(v))
-        game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
-    end
+        Title = "Zinc Hub | Redeem Codes",
+        Text = 'Updated on: 01/08/2024'
+    })
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Zinc Hub | Redeem Codes",
+        Text = 'Redeeming '.. tostring(v)
+    })
+    
+local args = {
+    [1] = "EnterPromoCode",
+    [2] = tostring(v)
+}
+print(tostring(v))
+game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
+end
